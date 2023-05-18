1ca68ecefce6a333789dccaa5c87d7d0cffe91f6
htl-donaustadt-informatik <lutz@htl-donaustadt.at>
2023-05-18 17:11:02 +0200

    Add project files.

---
+++ GitTest/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace GitTest
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Creating the first commit!");
+
+        }
+    }
+}
